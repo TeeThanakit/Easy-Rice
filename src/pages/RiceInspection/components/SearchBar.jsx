878fceab445090d463b04d5c04cb9c6603fafc3a
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-11-11 10:46:25 +0700

    feat: add search on date

--- src/pages/RiceInspection/components/SearchBar.jsx
+++ src/pages/RiceInspection/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Flex } from "antd"
+import { Button, Form, Input, Flex, DatePicker } from "antd"
 import { clearSearchBar } from "../riceInspectionSlice"
 import { useDispatch } from "react-redux"
 import { SearchOutlined } from "@ant-design/icons"
@@ -20,15 +20,25 @@ export default function SearchBar() {
 	async function clearData() {
 		form.setFieldsValue({
 			id: "",
+			formDate:"",
+			toDate:"",
 		})
 		dispatch(clearSearchBar())
 	}
 
 	return (
 		<Form form={form} onFinish={onSubmit} layout="vertical">
-			<Form.Item label="ID" name="id">
-				<Input placeholder="Search with ID" />
-			</Form.Item>
+			<Flex style={{ display: "flex", width: "100%", gap: "16px" }}>
+				<Form.Item label="ID" name="id" style={{ flex: 1 }}>
+					<Input placeholder="Search with ID" />
+				</Form.Item>
+				<Form.Item label="Form date" name="formDate" style={{ flex: 1 }}>
+					<DatePicker placeholder="Please select Form date" style={{ width: "100%" }} format="DD-MM-YYYY"/>
+				</Form.Item>
+				<Form.Item label="To date" name="toDate" style={{ flex: 1 }}>
+					<DatePicker placeholder="Please select to date" style={{ width: "100%" }} format="DD-MM-YYYY"/>
+				</Form.Item>
+			</Flex>
 			<Flex justify="space-between">
 				<Form.Item>
 					<Button type="second" className="text-red-600 underline" onClick={() => clearData()}>
